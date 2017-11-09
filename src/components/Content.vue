63a5181adf33e844a339dff8a14ba0f8c7d45244
Indraprahastha <indraprahastha@rocketmail.com>
2017-11-09 22:29:14 +0700

    Done

---
+++ src/components/Content.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Content col-md-9">
+    <div id="pembatas" v-if="Navcon" class="jumbotron">
+      <h1>{{Navcon.judul}}</h1>
+      <p>{{Navcon.isi}}</a></p>
+    </div>
+    <div id="pembatas" v-else class="jumbotron">
+      <h3> Klik Navbar Untuk Melihat Data </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  props: ['Navcon'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#pembatas{
+  border-radius: 30px;
+  padding: 20px;
+}
+</style>
---
+++ src/components/Navcon.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="Navcon">
+    <Sidebar :Navcon='artikel' @summoned="dataLemparan"/>
+    <Content :Navcon='artikelLemparan'/>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+import Content from '@/components/Content'
+import axios from 'axios'
+export default {
+  name: 'Navcon',
+  components: {
+    Sidebar,
+    Content
+  },
+  data () {
+    return {
+      artikelLemparan: null,
+      artikel: []
+    }
+  },
+  methods: {
+    dapatkanArtikel () {
+      axios.get('http://localhost:3000/artikel').then((response) => {
+        this.artikel = response.data
+      }, (error) => {
+        console.log(error)
+      })
+    },
+    dataLemparan (isi) {
+      console.log(isi)
+      this.artikelLemparan = isi
+    }
+
+  },
+  // mounted: function (Navcon) {
+  //   console.log(Navcon)
+  //   this.fetchArticles = Navcon
+  // },
+  created () {
+    this.dapatkanArtikel()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#border{
+  border-radius: 500px;
+}
+</style>
